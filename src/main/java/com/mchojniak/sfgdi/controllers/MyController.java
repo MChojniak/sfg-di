1a4ec76bee56c9ba19b32dcfcb3c3a2bf3a96c20
MChojniak <marcin.chojniak3@gmail.com>
2021-06-07 00:09:42 +0200

    Primary service using @Primary annotation

--- src/main/java/com/mchojniak/sfgdi/controllers/MyController.java
+++ src/main/java/com/mchojniak/sfgdi/controllers/MyController.java
@@ -1,12 +1,17 @@
 package com.mchojniak.sfgdi.controllers;
 
+import com.mchojniak.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
-    public String sayHello() {
-        System.out.println("Hello World!!");
+    private final GreetingService greetingService;
 
-        return "Hi folks";
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
     }
 }
