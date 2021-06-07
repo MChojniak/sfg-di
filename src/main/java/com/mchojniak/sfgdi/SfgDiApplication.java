e8cacc5ebbcd026958410bc36dbb9c2bfd62ffde
MChojniak <marcin.chojniak3@gmail.com>
2021-06-07 19:50:39 +0200

    Profile example

--- src/main/java/com/mchojniak/sfgdi/SfgDiApplication.java
+++ src/main/java/com/mchojniak/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package com.mchojniak.sfgdi;
 
-import com.mchojniak.sfgdi.controllers.ConstructorInjectedController;
-import com.mchojniak.sfgdi.controllers.MyController;
-import com.mchojniak.sfgdi.controllers.PropertyInjectedController;
-import com.mchojniak.sfgdi.controllers.SetterInjectedController;
+import com.mchojniak.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,7 +9,13 @@ import org.springframework.context.ApplicationContext;
 public class SfgDiApplication {
 
     public static void main(String[] args) {
+
+
         ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+
+        System.out.println(i18nController.sayHello());
+
         System.out.println("------------ Primary bean");
         MyController myController = (MyController) ctx.getBean("myController");
         System.out.println(myController.sayHello());
